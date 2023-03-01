ffe9e74a622a63b70ddefc4933f592f61ef3acaa
Erika Flores <erika.flores@bahia360.mx>
2023-03-01 15:00:33 -0600

    feat | create components Home, Content, Sidebar

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from "styled-components";
+import Sidebar from "../components/Sidebar";
+import Content from "../components/Content";
+
+const HomeWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+const Home = () => {
+    return (
+        <HomeWrapper>
+            <Sidebar/>
+            <Content/>
+
+        </HomeWrapper>
+    );
+};
+
+export default Home;
